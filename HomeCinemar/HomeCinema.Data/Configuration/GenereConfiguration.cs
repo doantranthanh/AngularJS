2cd5373b2dd5545f34b9c93857f33108c8111c1c
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-09-28 14:12:57 +0100

    Home Cinema Angular JS Web API

---
+++ HomeCinemar/HomeCinema.Data/Configuration/GenereConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Configuration
+{
+    public class GenereConfiguration : EntityBaseConfiguration<Genre>
+    {
+    }
+}
