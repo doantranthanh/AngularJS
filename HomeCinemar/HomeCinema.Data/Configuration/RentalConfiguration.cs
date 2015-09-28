1d22134a3512688f1b135286cc48a0a6969c8ae7
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-09-28 14:48:22 +0100

    UserRoleConfiguration

--- HomeCinemar/HomeCinema.Data/Configuration/RentalConfiguration.cs
+++ HomeCinemar/HomeCinema.Data/Configuration/RentalConfiguration.cs
@@ -11,8 +11,8 @@ namespace HomeCinema.Data.Configuration
     {
         public RentalConfiguration()
         {
-            Property(r => r.CustomerId).IsRequired(); 
-            Property(r => r.StockId).IsRequired(); 
+            Property(r => r.CustomerID).IsRequired(); 
+            Property(r => r.StockID).IsRequired(); 
             Property(r => r.Status).IsRequired().HasMaxLength(10); 
             Property(r => r.ReturnedDate).IsOptional();
         }
