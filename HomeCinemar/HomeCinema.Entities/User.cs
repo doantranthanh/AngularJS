4bd033ecec0bafbb873ce094924415953f62672a
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-09-28 14:50:32 +0100

    UserConfiguration

--- HomeCinemar/HomeCinema.Entities/User.cs
+++ HomeCinemar/HomeCinema.Entities/User.cs
@@ -18,6 +18,8 @@ namespace HomeCinema.Entities
         public string Email { get; set; }
         public string HashedPassword { get; set; }
         public string Salt { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime DateCreated { get; set; }
 
         public virtual ICollection<UserRole> UserRoles { get; set; }
     }
