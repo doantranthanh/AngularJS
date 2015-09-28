4c051d84230a627394600c3ccf3231c155ecce81
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-09-28 14:35:16 +0100

    Movie Configuration

--- HomeCinemar/HomeCinema.Data/Configuration/GenereConfiguration.cs
+++ HomeCinemar/HomeCinema.Data/Configuration/GenereConfiguration.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeCinema.Entities;
 
 namespace HomeCinema.Data.Configuration
 {
     public class GenereConfiguration : EntityBaseConfiguration<Genre>
     {
+        public GenereConfiguration()
+        {
+            Property(g => g.Name).IsRequired().HasMaxLength(50);
+        }
     }
 }
---
+++ HomeCinemar/HomeCinema.Data/Configuration/MovieConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeCinema.Entities;
+
+namespace HomeCinema.Data.Configuration
+{
+    public class MovieConfiguration : EntityBaseConfiguration<Movie>
+    {
+        public MovieConfiguration()
+        {
+            Property(m => m.Title).IsRequired().HasMaxLength(100);
+            Property(m => m.GenreID).IsRequired();
+            Property(m => m.Director).IsRequired().HasMaxLength(100);
+            Property(m => m.Writer).IsRequired().HasMaxLength(50);
+            Property(m => m.Producer).IsRequired().HasMaxLength(50);
+            Property(m => m.Writer).HasMaxLength(50);
+            Property(m => m.Producer).HasMaxLength(50);
+            Property(m => m.Rating).IsRequired(); 
+            Property(m => m.Description).IsRequired().HasMaxLength(2000); 
+            Property(m => m.TrailerURI).HasMaxLength(200);
+
+            // Configuration One To Many Relationship
+            HasMany(m => m.Stocks).WithRequired().HasForeignKey(s => s.MovieId);
+        }
+    }
+}
