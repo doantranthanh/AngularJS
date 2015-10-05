1ca1df90a6901ac9198ce8c856ee5a6edbd861c3
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-10-05 15:34:34 +0100

    Adding Authentication service to web

---
+++ HomeCinemar/HomeCinema.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using AutoMapper;
+
+namespace HomeCinema.Web.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            // Configure Autoface
+            AutofacWebapiConfig.Initialize(GlobalConfiguration.Configuration);
+
+            // Configre AutoMapper
+            //AutoMapperConfiguration.Configure();
+        }
+    }
+}
---
+++ HomeCinemar/HomeCinema.Web/Infrastructure/Extensions/RequestMessageExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Runtime.Remoting.Services;
+using System.Web.Http.Dependencies;
+using HomeCinema.Services.Abstract;
+
+namespace HomeCinema.Web.Infrastructure.Extensions
+{
+    public static class RequestMessageExtension
+    {
+        internal static IMembershipService GetMembershipService(this HttpRequestMessage request)
+        {
+            return request.GetService<IMembershipService>();
+        }
+
+        private static TService GetService<TService>(this HttpRequestMessage request)
+        {
+            IDependencyScope dependencyScope = request.GetDependencyScope();
+
+            TService service = (TService) dependencyScope.GetService(typeof (TService));
+
+            return service;
+        }
+         
+    }
+}
